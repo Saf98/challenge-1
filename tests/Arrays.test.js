8a1574202e177585b6f75ad959e2ca87d5a31162
Saf98 <sbilimoria98@gmail.com>
2025-01-20 16:59:02 +0000

    mixed values

--- tests/Arrays.test.js
+++ tests/Arrays.test.js
@@ -3,7 +3,7 @@ const removeDuplicates = require('../src/Arrays.js');
 
 
 describe('duplicates', () => {
-  it('Return Duplicated in an Array', () => {
+  it('Return duplicates in an Array', () => {
     const arr = [1, 2, 3, 2, 4, 1];
     const duplicateArray = [2, 1];
     expect(findDuplicates(arr)).toEqual(expect.arrayContaining(duplicateArray));
@@ -12,6 +12,18 @@ describe('duplicates', () => {
   it('Remove duplicates in an Array', () => {
     const arr = [1, 2, 3, 2, 4, 1];
     const singleArray = [1, 2, 3, 4];
+    expect(removeDuplicates(arr)).toEqual(expect.arrayContaining(singleArray));
+  });
+
+  it('Return mixed duplicates in an Array', () => {
+    const arr = ["a", 2, 3, "a", "b", 3];
+    const duplicateArray = ["a", 3];
+    expect(findDuplicates(arr)).toEqual(expect.arrayContaining(duplicateArray));
+  });
+
+  it('Remove mixed duplicates in an Array', () => {
+    const arr = ["a", 2, 3, "a", "b", 3];
+    const singleArray = ["a", 2, 3, "b"];
     expect(removeDuplicates(arr)).toEqual(expect.arrayContaining(singleArray));
   });
 });
