0d5534ca901c89d47981c4d76e4031a5d7a1c92f
Saf98 <sbilimoria98@gmail.com>
2025-01-28 16:20:20 +0000

    test things

--- src/StringReverser.js
+++ src/StringReverser.js
@@ -2,4 +2,17 @@ function reverseString(str) {
     return str.split("").reverse().join("");
 }
 
+
+const regexBoundary = /\b/;
+const special = /[^a-zA-Z0-9]/g;
+const arr = [];
+const str3 = str.split(regexBoundary).map((el, i) => {
+  
+  console.log(el.split(special));
+  
+//     console.log(el.split().splice(0))
+//     console.log(el.indexOf(","))
+});
+
+console.log(str3);
 module.exports = {reverseString};
