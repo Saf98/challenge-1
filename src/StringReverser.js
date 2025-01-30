7b7466d0d03a1813e5b8815d050616eafca44f8e
Saf98 <sbilimoria98@gmail.com>
2025-01-30 16:57:44 +0000

    mess around

--- src/StringReverser.js
+++ src/StringReverser.js
@@ -15,4 +15,49 @@ const str3 = str.split(regexBoundary).map((el, i) => {
 });
 
 console.log(str3);
+
+
+const arr = ["s","t", "d", " ", "c", "n", "'","t"];
+// const arr = "stdcn't";
+
+//   const idx = arr.indexOf("'"); // found element at pos 6
+
+// let newArr = [];
+// let newStr = [];
+
+// const str = arr.filter((e, id) => {
+//   const idx = arr.indexOf("'"); // found element at pos 6
+//   if (id !== idx) {
+//     newStr.reverse().push(e);
+    
+//     return newArr = newStr.toSpliced(idx, 0, "'");
+
+//   }
+// })
+
+
+function reverse(txt, match) {
+  let newArr = [];
+  let newStr = [];
+  const str = txt.filter((e, id) => {
+    const idx = txt.indexOf(match); // found element at pos 6
+    
+    if (id !== idx) {
+      newStr.reverse().push(e);
+
+      return newArr = newStr.toSpliced(idx, 0, "'");
+
+    }
+  });
+  return newArr.join("");
+
+}
+
+console.log(reverse(arr, "'"));
+// console.log(newStr.splice(idx, 0, "'"));
+// console.log(newStr);
+// console.log(str)
+
+// console.log(newStr.toSpliced(idx, 0, "'"));
+
 module.exports = {reverseString};
